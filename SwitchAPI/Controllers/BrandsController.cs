680992d870fd3ba689349f61178a7010b57bf5ea
unknown <saraniazkhani.811012@gmail.com>
2024-01-06 19:29:38 +0330

    finish

--- SwitchAPI/Controllers/BrandsController.cs
+++ SwitchAPI/Controllers/BrandsController.cs
@@ -49,18 +49,58 @@ namespace SwitchAPI.Controllers
             }
         }
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(string id, [FromBody] Cars updatedCar)
+        public async Task<IActionResult> Update(string id, [FromBody] CarsFiltered updatedCar)
         {
-            var collection = _mongoContext.GetCollection<Cars>("Brands");
+            var collection = _mongoContext.GetCollection<CarsFiltered>("Filter");
             try
             {
-                var filter = Builders<Cars>.Filter.Eq(x => x._id, id);
+                var filter = Builders<CarsFiltered>.Filter.Eq(x => x._id, id);
 
-                var update = Builders<Cars>.Update
-                    .Set(x => x.title, updatedCar.title);
+                var existingCar = await collection.Find(filter).FirstOrDefaultAsync();
 
-                var result = await collection.UpdateOneAsync(filter, update);
+                if (existingCar == null)
+                {
+                    return NotFound($"داده‌ای با این شناسه یافت نشد");
+                }
 
+                // جایگزین کردن فیلدها با اطلاعات جدید ارسال شده
+                if (!string.IsNullOrEmpty(updatedCar.brandname))
+                {
+                    existingCar.brandname = updatedCar.brandname;
+                }
+
+                if (!string.IsNullOrEmpty(updatedCar.modelname))
+                {
+                    existingCar.modelname = updatedCar.modelname;
+                }
+                if (updatedCar.year != 0)
+                {
+                    existingCar.year = updatedCar.year;
+                }
+                if (updatedCar.price != 0)
+                {
+                    existingCar.price = updatedCar.price;
+                }
+                if (updatedCar.usagekilometere != 0)
+                {
+                    existingCar.usagekilometere = updatedCar.usagekilometere;
+                }
+                if (!string.IsNullOrEmpty(updatedCar.bodystatus))
+                {
+                    existingCar.bodystatus = updatedCar.bodystatus;
+                }
+                if (!string.IsNullOrEmpty(updatedCar.fueltype))
+                {
+                    existingCar.fueltype = updatedCar.fueltype;
+                }
+                if (!string.IsNullOrEmpty(updatedCar.gearboxtype))
+                {
+                    existingCar.gearboxtype = updatedCar.gearboxtype;
+                }
+
+                // سایر فیلدها را نیز به همین صورت چک کنید و جایگزین کنید
+
+                var result = await collection.ReplaceOneAsync(filter, existingCar);
 
                 if (result.ModifiedCount > 0)
                 {
@@ -68,7 +108,7 @@ namespace SwitchAPI.Controllers
                 }
                 else
                 {
-                    return NotFound($"داده‌ای با این شناسه  یافت نشد");
+                    return NotFound($"داده‌ای با این شناسه یافت نشد یا هیچ تغییری اعمال نشد");
                 }
             }
             catch (Exception ex)
@@ -76,6 +116,42 @@ namespace SwitchAPI.Controllers
                 return StatusCode(500, $"خطا در به‌روزرسانی داده: {ex.Message}");
             }
         }
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Update(string id, [FromBody] CarsFiltered updatedCar)
+        //{
+        //    var collection = _mongoContext.GetCollection<CarsFiltered>("Filter");
+        //    try
+        //    {
+        //        var filter = Builders<CarsFiltered>.Filter.Eq(x => x._id, id);
+
+        //        var update = Builders<CarsFiltered>.Update
+        //            .Set(x => x.brandname, updatedCar.brandname)
+        //            .Set(x => x.modelname, updatedCar.modelname)
+        //            .Set(x => x.year, updatedCar.year)
+        //            .Set(x => x.usagekilometere, updatedCar.usagekilometere)
+        //            .Set(x => x.gearboxtype, updatedCar.gearboxtype)
+        //            .Set(x => x.bodystatus, updatedCar.bodystatus)
+        //            .Set(x => x.price, updatedCar.price)
+        //            .Set(x => x.fueltype, updatedCar.fueltype);
+
+        //        var result = await collection.UpdateOneAsync(filter, update);
+
+
+        //        if (result.ModifiedCount > 0)
+        //        {
+        //            return Ok($"داده با موفقیت به‌روزرسانی شد");
+        //        }
+        //        else
+        //        {
+        //            return NotFound($"داده‌ای با این شناسه  یافت نشد");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(500, $"خطا در به‌روزرسانی داده: {ex.Message}");
+        //    }
+        //}
 
 
     }
